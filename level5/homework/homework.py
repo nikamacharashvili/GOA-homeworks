810cf49086582aab25211921b878572d32123637
nikamachrashvili <nikolozmacharashvili331@gmail.com>
2024-06-01 14:35:31 +0400

    level 6

---
+++ level5/homework/homework.py
@@ -0,0 +1,34 @@
+ #homework1
+name = "nika" #შევქმენი ცვლადი სადაც შევინახე ჩმი სახელი.
+surname = "macharashvili"  #შევქმენი ცვლადი სადაც შევინახე ჩემი გვარი.
+age = 15    #შევქმენი ცვლადი სადაც შევინახე ჩემი ასაკი.
+height = 160.5 #შვქმენი ცვლადი სადაც შევინახე ჩემი სიმაღლე.
+adress = "shiraqis_qucha.N 102" #შვქმენი ცვლადი სადაც შევინახე ჩემი მისამართი.
+beautiful = "I am so hendsome" #შვქმენი ცვლადი სადაც შევინახე ჩემი დამახასიათებელი სიტყვა.
+family = "4 brothers,1 sister,mom and dad" #შვქმენი ცვლადი სადაც შევინახე ჩემი ოჯახის წევრების რაოდენობა.
+brother1 = "gio is 13 years old" #შვქმენი ცვლადი სადაც შევინახე ჩემი ძმის შსახებ ინფო.
+brother2 = "luka is 11 years old" #შვქმენი ცვლადი სადაც შევინახე ჩემი ძმის შსახებ ინფო.
+brother3 = "saba is 1 years old" #შვქმენი ცვლადი სადაც შევინახე ჩემი ძმის შსახებ ინფო.
+sister = "mari is 8 years old" #შვქმენი ცვლადი სადაც შევინახე ჩემი დის შსახებ ინფო.
+print("I am"+ " " + name + " " + surname + " " + "I am" + "" + str(age) + " " + "yers old" + " " + "I live in" + " " + "shiraqis_qucha.N 102" + " " +  "I am" + " " + str(height) + " " + "centimeter" + " " + beautiful + " " + "In my family are" + " " + family + " " + brother1 + " " + brother2 + " " + brother3 + " " + sister)
+ # დავპრინტე ჩემი ყველა მონაცემი
+#homework2
+#შევქმენით ცვლადები რომლის მნიშვნელობაც იყო integer
+number1 = 20 #
+number2 = 17 #
+number3 = 5 #
+number4 = 10 #
+number5 = 15 #
+number6 = 30 #
+number7 = 14 #
+number8 = 68 #
+number9 = 26 #
+number10 = 34 #
+#დავპრინტე მათი ჯამი
+print(number1 + number2)
+print( number10 - number2)
+print(number5 * number7)
+print(number1 / number3)
+print(number4 + number6)
+print(number8 / number10)
+print(number8 - number9)
---
+++ level6/homework/homework.py
@@ -0,0 +1,28 @@
+#homework2
+num1 = int(input("gtxovt shemoitanet ricxvi:")) #შემოვატანინეთ მომხმარებელს 2 რიცხვი.
+num2 = int(input("gtxovt shemoitanet ricxvi:"))
+print(num1 + num2) #ამ ორ რიცხვს შორის შევასრულეთ მიმატება.
+print(num1 - num2) #ამ ორ რიცხვს შორის შევასრულეთ გამოკლება.
+print(num1 * num2) #ამ ორ რიცხვს შორის შევასრულეთ გამრავლება.
+print(num1 / num2) #ამ ორ რიცხვს შორის შევასრულეთ გაყოფა.
+
+#homewor3
+name = input("gtxovt shemoitanet tqveni saxeli:")  #შემოვატანინეთ მომხმარებელს თავისი სახელი.
+print("gilocav dabadebis dges" + " " + name ) #დავუწერეთ მისალოცი ბარათი თავის სახელზე.
+
+#homework4
+name = input("gtxovt shemoitanet tqveni saxeli:") #შემოვატანინეთ მომხმარებელს თავისი სახელი.
+surname = input("gtxovt shmoitanet tqveni gvari:") #შემოვატანინეთ მომხმარებელს თავისი გვარი.
+age = input("gtxovt shmoitanet tqveni asaki:") #შემოვატანინეთ მომხმარებელს თავისი ასაკი.
+print(name,surname,age) #დავპრინტეთ მათ შმოტანილი მონაცემები.
+
+#homework5
+mom_age = int(input("gtxovt shmoitanet tqveni dedis asaki:")) #შემოვატანინეთ მომხმარებელს თავისი დედის ასაკი.
+dad_age = int(input("gtxovt shemoitanet tqveni mamis asaki:")) #შემოვატანინეთ მომხმარებელს თავისი მამის ასაკი.
+brother = int(input("gtxovt shmoitanet tqveni zmis asaki:")) #შემოვატანინეთ მომხმარებელს თავისი ძმის ასაკი.
+sister = int(input("gtxovt shmoitanet tqveni dis asaki")) #შემოვატანინეთ მომხმარებელს თავისი დის ასაკი.
+print(mom_age + 25 ) #დავპრინტეთ რამდენი წლის იქნებოდა მათი დედა 25 წლის შემდეგ (მაგალითისთვის ჩავწერე დედაჩემის)
+print(dad_age + 25) #დავპრინტეთ რამდენი წლის იქნებოდა მათი მამა 25 წლის შემდეგ (მაგალითისთვის ჩავწერე მამაჩემის)
+print(brother + 25) #დავპრინტეთ რამდენი წლის იქნებოდა მათი ძმა 25 წლის შემდეგ (მაგალითისთვის ჩავწერე ჩემი ძმის)
+print(sister + 25) #დავპრინტეთ რამდენი წლის იქნებოდა მათი და 25 წლის შემდეგ (მაგალითისთვის ჩავწერე ჩემი დის)
+
